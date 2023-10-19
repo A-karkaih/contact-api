ac3db0e153400e313b9844f70540d30449e83386
A-karkaih <akarkaih@gmail.com>
2023-10-19 20:15:31 +0100

    Users model qnd pqssword hashing

--- cntrollers/usersController.js
+++ cntrollers/usersController.js
@@ -1,11 +1,46 @@
 const asyncHandler = require("express-async-handler");
-
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 //@Desc Register a user
 //@route  Post /api/users/register
 //@access  public
 
 const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
 
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+
+    const userAvailable = await User.findOne({ email });
+
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User Already Register!");
+    }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("hashed paswword is ", hashedPassword);
+    
+    const newUser = await User.create({
+        username,
+        email,
+        password: hashedPassword ,
+        
+    }); 
+
+    if (newUser) {
+        res.status(201).json({_id : newUser.id , email : newUser.email})
+    }
+    else {
+        res.status(400);
+        throw new Error("User data not valid");
+    }
+
+   
+   
     res.json({ message: "Register the user" })
 
 });
