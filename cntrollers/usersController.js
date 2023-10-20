102b03e509966834c48684b25466c7babe32ca3f
A-karkaih <akarkaih@gmail.com>
2023-10-20 19:02:15 +0100

    JWT and protecting routes

--- cntrollers/usersController.js
+++ cntrollers/usersController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 //@Desc Register a user
 //@route  Post /api/users/register
 //@access  public
 
 const registerUser = asyncHandler(async (req, res) => {
+
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
@@ -51,13 +53,43 @@ const registerUser = asyncHandler(async (req, res) => {
 
 const loginUser = asyncHandler(async (req, res) => {
 
-    res.json({ message: "Login the user" })
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+    //compare pasword with hashedpassword 
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id ,
+            }
+        }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn:"1m"
+        });
+
+
+        res.status(200).json({accessToken});
+
+    }
+    else {
+        res.status(401);
+        throw new Error("email or password not valid ");
+    }
+
+
+
+    
 
 });
 
 //@Desc Cureent  user info
 //@route  get /api/users/current
-//@access  rivate
+//@access  private
 
 const currentUser = asyncHandler(async (req, res) => {
 
